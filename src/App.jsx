f6d01d62cfd7e974fa51e16a9f0041e828f2c925
raza-abbas-23sw <raza84abbas@gmail.com>
2025-05-14 12:56:11 +0500

    ceated pages and added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import AppRoutes from "./routes/AppRoutes";
 function App() {
   return (
     <>
       <Navbar/>
+      <main className="pt-20 px-4 bg-amber-300">
+        <AppRoutes />
+      </main>
     </>
   );
 }
